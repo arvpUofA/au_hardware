65d4a384417acf85ce742b8f9c065e05914d98e9
calef800 <brendancalef@gmail.com>
2017-06-05 19:55:14 -0600

    Fixed code. Sensor works :)

--- Environment_sensor_test.ino
+++ Environment_sensor_test.ino
@@ -6,8 +6,8 @@ unsigned char buf[4];
 
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin(SENSORADDRESS);
-  Serial.begin(9600);
+  Wire.begin();
+  Serial.begin(115200);
   Serial.print("Starting the temperature and humimdity sensor");
 
 }
@@ -15,14 +15,22 @@ void setup() {
 void loop() {
   sensetemp=0;
   sensehum=0;
+
+  Wire.beginTransmission(SENSORADDRESS);
+  Wire.write(SENSORADDRESS<<1);
+  Wire.endTransmission();
+  delay(100);
+
+  
   Wire.requestFrom(SENSORADDRESS, 4);
+  
   if (Wire.available()==4){
     for (int i = 0; i <4; i++){
       buf[i]=Wire.read();
       Serial.println(buf[i]);
     }
-    if(!bitRead(buf[0], 7)&&!bitRead(buf[0], 6)){
-      Serial.println("Status is normal");
+    if(!bitRead(buf[0], 7)&&bitRead(buf[0], 6)){
+      Serial.println("Status is stale");
     }
     sensehum += (buf[0] << 8) + buf[1];
     Serial.println(humidity(sensehum));
@@ -31,14 +39,14 @@ void loop() {
   } else {
     Serial.print("Error in data.\n");
   }
-  delay(50);
+  delay(1000);
 }
 float humidity(float hum){
    float humidity = float(hum)/16382*100;
    return humidity;
 }
 float temp(float tem){
-    float temperature = tem/16382*(165-40);
+    float temperature = (tem/16382*165)-40;
     return temperature;
 }
 
